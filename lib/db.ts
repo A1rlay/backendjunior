f1415384cbe9045a51edbacd657eb2aca284df97
a1rlay <abdelink@hotmail.com>
2026-07-26 16:47:59 -0600

    Created transactions table

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,6 @@
 import Database from "better-sqlite3";
 const db = new Database("auth.db");
+db.pragma("foreign_keys = ON");
 
 db.exec(`
 	CREATE TABLE IF NOT EXISTS users(
@@ -15,6 +16,17 @@ db.exec(`
 		token TEXT PRIMARY KEY,
 		user_id INTEGER NOT NULL,
 		expires_at TEXT NOT NULL,
+		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+	)
+`);
+
+db.exec(`
+	CREATE TABLE IF NOT EXISTS transactions(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER NOT NULL,
+		amout INTEGER NOT NULL,
+		type TEXT NOT NULL,
+		created_at TEXT NOT NULL DEFAULT (datetime('now')),
 		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 	)
 `);
